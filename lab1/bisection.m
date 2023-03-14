a2bdcdb0bdec59f8b44134af61285acf36e9f7d4
Shuva <38221577+shin890@users.noreply.github.com>
2023-03-14 22:47:10 +0600

    Add files via upload

---
+++ lab1/bisection.m
@@ -0,0 +1,30 @@
+tic
+x1=-3;
+x2=1;
+E=0.0001;
+n=0;
+f1=fnc(x1);
+f2=fnc(x2);
+
+if (f1*f2)>0
+    return
+else
+    while (abs((x2-x1)/x2))>E
+        x0=(x1+x2)/2;
+        f0=fnc(x0);
+        if (f1*f0)<0
+            x2=x0;
+            f2=f0;
+        else
+            x1=x0;
+            f1=f0;
+        end
+        n=n+1;
+
+    end
+end
+disp(x0);
+disp(n);
+toc
+return;
+
---
+++ lab1/fnc.m
@@ -0,0 +1,3 @@
+function y=fnx(x)
+y=x^2+x-2;
+end
